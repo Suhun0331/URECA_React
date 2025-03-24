99f0ef66883285c5e8625c47ec9da1cd662a2b5c
Suhun0331 <rlatngns6046@naver.com>
2025-03-24 14:02:34 +0900

    create app1

---
+++ src/home/footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer(){
+
+    return(
+        <div class="jumbotron">
+            <h1>Footer</h1>
+            <p>Bootstrap is the most popular HTML, CSS...</p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/home/header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header(){
+
+    return(
+        <div class="jumbotron">
+            <h1>Header</h1>
+            <p>Bootstrap is the most popular HTML, CSS...</p>
+        </div>
+    );
+}
+
+export default Header;
